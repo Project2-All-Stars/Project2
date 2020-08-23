8f7a1c77ee19ed90c145517ef96c44c4d05fe552
kdemello1980 <k6demello28@fastmail.fm>
2020-08-23 14:11:27 -0400

    at this point it works as an empty application serving the index.jsp

--- src/main/java/com/rev/service/AbsenceController.java
+++ src/main/java/com/rev/service/AbsenceController.java
@@ -1,4 +1,9 @@
 package com.rev.service;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/absences")
 public class AbsenceController {
 }
