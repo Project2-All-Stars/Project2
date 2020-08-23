edfd41f12f912bfc9633be10fda513c3c68d9864
kdemello1980 <k6demello28@fastmail.fm>
2020-08-23 14:54:34 -0400

    at this point the absences controller initializes and starts serving.  I'll be changing to my own rds from this point as to not interfere with Trevor's DAO work

--- src/main/java/com/rev/service/AbsenceController.java
+++ src/main/java/com/rev/service/AbsenceController.java
@@ -1,9 +1,23 @@
 package com.rev.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 @RequestMapping("/absences")
 public class AbsenceController {
+    
+    @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseBody
+    public ResponseEntity<String> getAllAbsences() {
+        return new ResponseEntity<String>( "This is the response", HttpStatus.OK);
+    }
+    
 }
