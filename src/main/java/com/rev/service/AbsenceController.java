c57737807d537966e198da02b8c1f47b166588a3
kdemello1980 <k6demello28@fastmail.fm>
2020-08-24 12:38:57 -0400

    pushing latest

--- src/main/java/com/rev/service/AbsenceController.java
+++ src/main/java/com/rev/service/AbsenceController.java
@@ -12,32 +12,35 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.rev.model.Absence;
 
 import java.lang.reflect.Type;
 import java.util.List;
 
-
+/**
+ *
+ */
 @Controller
 @RequestMapping("/absences")
 public class AbsenceController {
     private Gson gson = new Gson().newBuilder().enableComplexMapKeySerialization().create();
-	Type tAbsenceList = new TypeToken<List<Absence>>() {}.getType();
+//	Type tAbsenceList = new TypeToken<List<Absence>>() {}.getType();
     GenericRepository<Absence> absenceRepository;
 
 	@Autowired
 	public void setAbsenceRepository(GenericRepository<Absence> absenceRepository) {
 		this.absenceRepository = absenceRepository;
 	}
+
+    /**
+     *
+     * @return
+     */
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<String> getAllAbsences() {
-//        return new ResponseEntity<String>( "This is the response", HttpStatus.OK);
         List<Absence> list = absenceRepository.findAll();
         if (list == null) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -45,5 +48,37 @@ public class AbsenceController {
             return new ResponseEntity<>(gson.toJson(list, List.class), HttpStatus.OK);
         }
     }
-    
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<String> getAbsenceById(@PathVariable int id){
+        Absence a = absenceRepository.findById(id);
+        return a == null ? new ResponseEntity<>(null, HttpStatus.NOT_FOUND) :
+                new ResponseEntity<>(gson.toJson(a, Absence.class), HttpStatus.OK);
+    }
+//
+//    /**
+//     *
+//     * @param id
+//     * @param absence
+//     * @return
+//     */
+//    @PutMapping(path = "{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+//    @ResponseBody
+//    public ResponseEntity<String> updateAbsence(@PathVariable int id, @RequestBody String absence){
+//        Absence a = absenceRepository.findById(id);
+//        if (a == null)
+//            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+//
+//        a = gson.fromJson(absence, Absence.class);
+//        a.setAid(id);
+//        return absenceRepository.update(a) ? new ResponseEntity<>(null, HttpStatus.OK) :
+//            new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
+
 }
--- src/test/java/tests/com/rev/service/AbsenceControllerTest.java
+++ src/test/java/tests/com/rev/service/AbsenceControllerTest.java
@@ -4,7 +4,6 @@ package tests.com.rev.service;
 import com.rev.dao.GenericRepository;
 import com.rev.model.Absence;
 import com.rev.service.AbsenceController;
-import org.checkerframework.checker.units.qual.A;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,7 +12,6 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -21,10 +19,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -39,11 +33,11 @@ import static org.mockito.BDDMockito.given;
 //@PropertySource("classpath:/WEB-INF/application.properties")
 @WebAppConfiguration
 public class AbsenceControllerTest {
+//	private Gson gson = new Gson().newBuilder().enableComplexMapKeySerialization().create();
 
     @Autowired
     WebApplicationContext wac;
 
-//    @Autowired
     private MockMvc mockMvc;
 
     @Mock
@@ -52,30 +46,29 @@ public class AbsenceControllerTest {
     @InjectMocks
     private GenericRepository<Absence> repo;
 
-//    private List<Absence> absenceList;
-
     @Before
     public void setup(){
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
     }
 
-//    @BeforeEach
-//    public void setupList(){
-//        this.absenceList  = new ArrayList<Absence>();
-//        absenceList.add(new Absence(1, new Date(System.currentTimeMillis()), 1));
-//        absenceList.add(new Absence(2, new Date(System.currentTimeMillis()), 2));
-//        absenceList.add(new Absence(3,new Date(System.currentTimeMillis()), 1));
-//    }
 
     @Test
     public void testAllAbsences() throws Exception {
-//        given(repo.findAll()).willReturn(absenceList);
         MvcResult result = mockMvc.perform(get("/absences"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
         Assert.assertNotNull(result);
         Assert.assertTrue(result.getResponse().getContentAsString().length() > 0);
-//        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testAbsenceById() throws Exception {
+        int id = 1;
+        MvcResult result = mockMvc.perform(get("/absences/" + id))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        Assert.assertNotNull(result);
     }
 }
